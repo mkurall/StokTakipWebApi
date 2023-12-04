aadd60777332a711598648ef8959c0d716b67b84
Mustafa KURAL <mustafakural@outlook.com>
2023-12-04 12:11:29 +0300

    Add project files.

---
+++ Controllers/StokTakipController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StokTakipWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[action]")]
+    public class StokTakipController : Controller
+    {
+        [HttpGet]
+        public string Test()
+        {
+            return "Api ile Bağlantı çalıştı";
+        }
+    }
+}
